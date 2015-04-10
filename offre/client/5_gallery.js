f85848f5015ee41bd70615da0d449ce26dd7df64
claudeaubry <claude.aubry@gmail.com>
2015-04-10 17:14:06 +0200

    phrases qui vont bien pour gymkhana

--- offre/client/5_gallery.js
+++ offre/client/5_gallery.js
@@ -1,29 +1,29 @@
 items = {
   top: [{
-      title: "Les premiers pas d’une aventure",
+      title: "Les premiers pas d'un voyage vers l'agilité",
       img: "premiers-pas-dune-aventure.jpg"
     },{
-      title: "Collaborer et co-construire",
+      title: "Collaborer et co-construire avec les citoyens",
       img: "collaborer-et-coconstruire.jpg"
     },{
-      title: "Découvrir des endroits insolites",
+      title: "Découvrir des idées nouvelles et les essayer",
       img: "decouvrir-des-endroits-insolites.jpg"
     },{
-      title: "Activer la page blanche ensemble",
+      title: "Remplir la page blanche ensemble, collectivités et citoyens",
       img: "activer-page-blanche-ensemble.jpg"
     }
   ],
   bottom: [{
-      title: "Explorer votre inconnu",
+      title: "Voyager léger à la rencontre des citoyens",
       img: "explorer-votre-inconnu.jpg"
     },{
-      title: "Laisser infuser ses idées",
+      title: "Filtrer les idées pour ne garder que les bonnes",
       img: "laisser-infuser-ses-idees.jpg"
     },{
-      title: "Marcher pour réfléchir",
+      title: "Ajuster régulièrement sa trajectoire pour plus d'impact",
       img: "marcher-pour-reflechir.jpg"
     },{
-      title: "Tester uniquement l’essentiel",
+      title: "Garder uniquement l’essentiel, oublier le superflu",
       img: "tester-uniquement-lessentiel.jpg"
     }
   ]
